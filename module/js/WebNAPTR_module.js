d0cd3ac32942ea9f02491a2bf527b0456bde778a
kanei <marek.vantuch@gmail.com>
2010-07-28 16:48:43 +0200

    Added few more services, commit before rewritting functions to different function system

--- module/js/WebNAPTR_module.js
+++ module/js/WebNAPTR_module.js
@@ -1,43 +1,43 @@
 (function($) {
-  $(document).ready(function () {
+   $(document).ready(function () {
 
-    // replace default submit function
-    $(".form-submit").click(function(){
-      var result, uri, tel;
-      var selected = $('#edit-services input:radio:checked').val();
-      var flags = $('#flags_fs input:radio:checked').val();
-      Webnaptr.setFlags(flags);
+      // replace default submit function
+      $(".form-submit").click(function(){
+         var result, uri, tel;
+         var selected = $('#edit-services input:radio:checked').val();
+         var flags = $('#flags_fs input:radio:checked').val();
+         Webnaptr.setFlags(flags);
       
-      switch(selected){
-      case '1':
-        uri = $('#edit-sip-uri').val();
-        tel = $('#edit-sip-telnum').val();
-        result = Webnaptr.generate_e2u_sip(uri, tel);
-        break;
-      case '2':
-        uri = $('#edit-sip-uri').val();
-        tel = $('#edit-sip-telnum').val();
-        result = Webnaptr.generate_e2u_email(uri, tel);
-        break;
-      default:
-        //Error message
-        break;
-      }
+         switch(selected){
+            case '1':
+               uri = $('#edit-sip-uri').val();
+               tel = $('#edit-sip-telnum').val();
+               result = Webnaptr.generate_e2u_sip(uri, tel);
+               break;
+            case '2':
+               uri = $('#edit-sip-uri').val();
+               tel = $('#edit-sip-telnum').val();
+               result = Webnaptr.generate_e2u_email(uri, tel);
+               break;
+            default:
+               //Error message
+               break;
+         }
 
-      $("#edit-result").val(result);
+         $("#edit-result").val(result);
 
-      return false;
-    });
+         return false;
+      });
 
-    $("#edit-order").change(function(){
-      Webnaptr.setOrder(this.value);
-    });
+      $("#edit-order").change(function(){
+         Webnaptr.setOrder(this.value);
+      });
 
-    $("#edit-preference").change(function(){
-      Webnaptr.setPreference(this.value);
-    })
+      $("#edit-preference").change(function(){
+         Webnaptr.setPreference(this.value);
+      })
     
     
-  });
+   });
 
 })(jQuery);
